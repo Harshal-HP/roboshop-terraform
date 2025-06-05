fb94b7f56593c5639464c3ccf783516ab8c0ee78
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-06 00:58:55 +0530

    test9

--- main.tf
+++ main.tf
@@ -5,18 +5,18 @@ module "resource-group" {
   location = each.value["location"]
 }
 
-module "databases" {
-  depends_on                          = [ module.resource-group ]
-  for_each                            = var.databases 
-  source                              = "./modules/vm"
-  name                                = each.key
-  location                            = module.resource-group[each.value["rgname"]].location
-  rg_name                             = module.resource-group[each.value].name
-  image_id                            = var.image_id
-  network_interface_id                = var.network_interface_id
-  zone                                = var.zone
-  azurerm_network_security_group_name =  var.azurerm_network_security_group_name
-}
+# module "databases" {
+#   depends_on                          = [ module.resource-group ]
+#   for_each                            = var.databases 
+#   source                              = "./modules/vm"
+#   name                                = each.key
+#   location                            = module.resource-group[each.value["rgname"]].location
+#   rg_name                             = module.resource-group[each.value].name
+#   image_id                            = var.image_id
+#   network_interface_id                = var.network_interface_id
+#   zone                                = var.zone
+#   azurerm_network_security_group_name =  var.azurerm_network_security_group_name
+# }
 
 # module "applications" {
 #   depends_on                          = [ module.databases ]
@@ -30,6 +30,6 @@ module "databases" {
 #   azurerm_network_security_group_name = var.azurerm_network_security_group_name
 # }
 
-output "print" {
-  value    = module.resource-group.name
+output "rg" {
+  value    = module.resource-group
 }
