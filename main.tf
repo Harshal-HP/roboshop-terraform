99930ae48070610e49461b9ed4b664e591340cd3
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-06 00:26:28 +0530

    test4

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-# module "resource-group" {
-#   for_each = var.rg_name
-#   source   = "./modules/resource-group"
-#   name  = "roboshop-${each.key}-${var.env}"
-#   location = each.value["location"]
-# }
+module "resource-group" {
+  for_each = var.rg_name
+  source   = "./modules/resource-group"
+  name  = "roboshop-${each.key}-${var.env}"
+  location = each.value["location"]
+}
 
 # module "databases" {
 #   for_each                            = var.databases 
