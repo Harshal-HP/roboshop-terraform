d7d4987ee1868c8b538dc8aee670c4156bc35335
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-08 17:32:39 +0530

    roboshop with hashicorp vault

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -68,7 +68,7 @@ resource "null_resource" "ansible" {
   provisioner "remote-exec" {
     inline = [
       "sudo dnf install python3.12 python3.12-pip -y",
-      "sudo pip3.12 install ansible",
+      "sudo pip3.12 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/harshal-09-p/roboshop-ansible roboshop.yml -e env=${var.env} -e app_name=${var.name} -e token=${var.token}"
     ]
   } 
