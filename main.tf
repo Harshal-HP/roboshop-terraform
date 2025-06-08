06169cbcef7addf893bfaf2e146bf7f436d1fc05
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-08 17:23:08 +0530

    roboshop with hashicorp vault

--- main.tf
+++ main.tf
@@ -23,17 +23,17 @@ module "databases" {
   env                                 = var.env
 }
 
-module "applications" {
-  depends_on                          = [ module.databases ]
-  for_each                            = var.applications
-  source                              = "./modules/vm"
-  name                                = each.key
-  location                            = module.resource-group[each.value["rgname"]].location 
-  rg_name                             = module.resource-group[each.value["rgname"]].name
-  image_id                            = var.image_id
-  network_interface_id                = var.network_interface_id
-  zone                                = var.zone
-  azurerm_network_security_group_id   =  var.azurerm_network_security_group_id
-  dns_record_gp_name                  = var.dns_record_gp_name
-  env                                 = var.env
-}
+# module "applications" {
+#   depends_on                          = [ module.databases ]
+#   for_each                            = var.applications
+#   source                              = "./modules/vm"
+#   name                                = each.key
+#   location                            = module.resource-group[each.value["rgname"]].location
+#   rg_name                             = module.resource-group[each.value["rgname"]].name
+#   image_id                            = var.image_id
+#   network_interface_id                = var.network_interface_id
+#   zone                                = var.zone
+#   azurerm_network_security_group_id   =  var.azurerm_network_security_group_id
+#   dns_record_gp_name                  = var.dns_record_gp_name
+#   env                                 = var.env
+# }
