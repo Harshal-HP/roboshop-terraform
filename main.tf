33ad8e3ab13095a90b678b1deb5fa1471275a5e0
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-22 12:25:11 +0530

    kubectl helm chart env roboshop

--- main.tf
+++ main.tf
@@ -5,25 +5,25 @@ module "resource-group" {
   location = each.value["location"]
 }
 
-# module "databases" {
-#   depends_on                          = [ module.resource-group ]
-#   for_each                            = var.databases
-#   source                              = "./modules/vm"
-#   name                                = each.key
-#   location                            = module.resource-group[each.value["rgname"]].location
-#   rg_name                             = module.resource-group[each.value["rgname"]].name
-# #  name                                = each.value["db_name"]
-# #  location                            = module.resource-group[each.key].location
-# #  rg_name                             = module.resource-group[each.key].name
-#   image_id                            = var.image_id
-#   network_interface_id                = var.network_interface_id
-#   zone                                = var.zone
-#   azurerm_network_security_group_id   = var.azurerm_network_security_group_id
-#   dns_record_gp_name                  = var.dns_record_gp_name
-#   env                                 = var.env
-#   token                               = var.token
-#   type                                = "db"
-# }
+module "databases" {
+  depends_on                          = [ module.resource-group ]
+  for_each                            = var.databases
+  source                              = "./modules/vm"
+  name                                = each.key
+  location                            = module.resource-group[each.value["rgname"]].location
+  rg_name                             = module.resource-group[each.value["rgname"]].name
+#  name                                = each.value["db_name"]
+#  location                            = module.resource-group[each.key].location
+#  rg_name                             = module.resource-group[each.key].name
+  image_id                            = var.image_id
+  network_interface_id                = var.network_interface_id
+  zone                                = var.zone
+  azurerm_network_security_group_id   = var.azurerm_network_security_group_id
+  dns_record_gp_name                  = var.dns_record_gp_name
+  env                                 = var.env
+  token                               = var.token
+  type                                = "db"
+}
 #
 # module "applications" {
 #   depends_on                          = [ module.databases ]
