0c4daf41e832a19927a5016802c08f188e695b35
harshal-09-p <harshal.phadatare@gmail.com>
2025-06-23 23:48:03 +0530

    update helm

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -1,3 +1,7 @@
+resource "null_resource" "kubeconfig" {
+    command = "az aks get-credentials --name=${var.names} --resource-group=${var.rg_name} --overwrite-existing"
+}
+
 resource "helm_release" "external-secrets" {
   name       = "external-secrets"
   repository = "https://charts.external-secrets.io"
@@ -6,8 +10,8 @@ resource "helm_release" "external-secrets" {
   create_namespace = true
   set = [
     {
-        name  = "installCRDs"
-        value = "true"
+      name  = "installCRDs"
+      value = "true"
     }
   ]
 }
