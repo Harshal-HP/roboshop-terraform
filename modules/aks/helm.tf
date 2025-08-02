d88ce9505a3960186bf619068e6b8d9426173971
harshal-09-p <harshal.phadatare@gmail.com>
2025-08-02 13:30:39 +0530

    commit

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -61,3 +61,18 @@ EOF
 TF
   }
 }
+
+resource "helm_release" "argocd" {
+  depends_on = [null_resource.kubeconfig]
+  name       = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argocd"
+  create_namespace = true
+  set = [
+    {
+      name  = "server.service.type"
+      value = "LoadBalancer"
+    }
+  ]
+}
